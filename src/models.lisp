d3ebb8b316a29d10f972f9d9c5600ec7238f172d
Javier Olaechea <pirata@gmail.com>
2013-05-04 11:15:43 -0500

    Use serial instead of intenger for primary keys

--- src/models.lisp
+++ src/models.lisp
@@ -6,14 +6,14 @@
      (:metaclass dao-class))
 
 (defclass definition ()
-  ((id :col-type integer :initarg id :accessor definition-id)
+  ((id :col-type serial :initarg id :accessor definition-id)
    (content :col-type string :initarg content :accessor definition-content)
    (word :col-type string :initarg word :accessor definition-word))
   (:metaclass dao-class)
   (:keys id))
 
 (defclass example ()
-  ((id :col-type integer :initarg id :accessor example-id)
+  ((id :col-type serial :initarg id :accessor example-id)
    (content :col-type string :initarg content :accessor example-content)
    (definition-id :col-type integer :initarg definition
                   :accessor example-definition))
