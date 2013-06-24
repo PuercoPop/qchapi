533c27f2084d3478c951efa69656db45a749f96b
Javier Olaechea <pirata@gmail.com>
2013-06-24 17:18:51 -0400

    Selecting DAOs

--- src/models/queries.lisp
+++ src/models/queries.lisp
@@ -1,9 +1,2 @@
 (in-package :qchapi.models)
 
-
-(defun test () 
-  (pomo:insert-dao (make-instance 'definition
-                                  :content "es un piticlin"
-                                  :word "bobo")))
-
-
--- src/models/utils.lisp
+++ src/models/utils.lisp
@@ -3,3 +3,7 @@
 (defmacro ensure-table (table-name)
   `(unless (pomo:table-exists-p ,table-name)
      (pomo:create-table ',table-name)))
+
+(defmacro ensure-dao (dao-instance)
+  `(unless (pomo:dao-exists-p ,dao-instance)
+     (pomo:insert-dao ,dao-instance)))
